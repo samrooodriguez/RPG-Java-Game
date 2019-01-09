cab4dbb56028186b36b4118eec62811db1478da5
Sam Rodriguez <sam27rodriguez@gmail.com>
2019-01-09 10:24:03 -0600

    first portion added with name and accept yes or no

--- src/Adventure.java
+++ src/Adventure.java
@@ -4,6 +4,14 @@ public class Adventure {
     public static void main(String[] arg){
         Scanner scanner = new Scanner(System.in).useDelimiter("\n");
 
-
+        System.out.println("Hello there, Welcome!" + "Enter your hero name:");
+        String hero = scanner.next();
+        System.out.println("Hello " + hero + "!" + " Do you choose to accept this adventure?");
+        String userInput = scanner.next();
+        if(userInput.contains("yes")){
+            System.out.println(" ");
+        }else{
+            System.out.println("Game over!");
+        }
     }
 }
